7d1663eb69e2e9e0ec3c65ef5797b9d2c81d8091
vishak45 <vishak.r565@gmail.com>
2025-05-27 20:27:42 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -148,13 +148,11 @@ function Home() {
                                   flag++,
                                   (
                                     <img
-                                      key={index}
-                                      src={`https://blogit-backend-nfpc.onrender.com/${image.imageUrl.replace(
-                                        /\\/g,
-                                        "/"
-                                      )}`}
-                                      alt={`Blog Image ${index + 1}`}
-                                    />
+  key={index}
+  src={image.imageUrl.replace(/\\/g, "/")}
+  alt={`Blog Image ${index + 1}`}
+/>
+
                                   )
                                 )
                               )
